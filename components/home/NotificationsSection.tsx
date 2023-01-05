b2aa26f0da75dc37413307451f66bd7661cd2b82
Basit Saeed <69200255+winepine@users.noreply.github.com>
2023-01-05 15:25:03 +0500

    Notific Realtime Updates

--- components/home/NotificationsSection.tsx
+++ components/home/NotificationsSection.tsx
@@ -1,7 +1,33 @@
 import { Box, Divider, Heading, HStack, Stack, Text } from "@chakra-ui/react";
+import {
+  collection,
+  onSnapshot,
+  Unsubscribe,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../../firebase/clientApp";
 import NotificationItem from "./NotificationItem";
 
 const NotificationSection = () => {
+  const [notifications, setNotifications] = useState<any[]>([]);
+  useEffect(() => {
+    const colRef = collection(db, "notifications");
+    const DBQuery = query(colRef, orderBy("timeStamp"));
+    const unsub: Unsubscribe = onSnapshot(DBQuery, snapshot => {
+      let Notifications: any[] = [];
+      snapshot.docs.forEach(doc => {
+        Notifications.push(doc.data());
+      });
+
+      setNotifications(Notifications);
+    });
+    return unsub;
+  }, []);
+  useEffect(() => {
+    console.log({ notifications });
+  }, [notifications]);
   return (
     <Stack w="50%" p={8} align="center">
       <Heading color="gray.600" fontWeight="semibold">
@@ -12,38 +38,15 @@ const NotificationSection = () => {
         <Text color="green">Live</Text>
       </HStack>
       <Divider py={4} />
-      <NotificationItem
-        notificationStatus="danger"
-        notificationText="SOS Signal Received (H#176-D)"
-      />
-      <NotificationItem
-        notificationStatus="success"
-        notificationText="Staff Entered"
-      />
-      <NotificationItem
-        notificationStatus="success"
-        notificationText="Staff Exited"
-      />
-      <NotificationItem
-        notificationStatus="danger"
-        notificationText="Jaidi House In Danger"
-      />
-      <NotificationItem
-        notificationStatus="danger"
-        notificationText="SOS Signal Received (H#176-D)"
-      />
-      <NotificationItem
-        notificationStatus="success"
-        notificationText="Staff Entered"
-      />
-      <NotificationItem
-        notificationStatus="danger"
-        notificationText="lorem ipsum this is a big notification to check kidr jata wrap hota ke nahi, bas pohanchne lage end pe, han hogya fine"
-      />
-      <NotificationItem
-        notificationStatus="success"
-        notificationText="Staff Entered"
-      />
+      <>
+        {notifications.map((notif, key) => (
+          <NotificationItem
+            notificationStatus={notif.type}
+            notificationText={notif.text}
+            key={key}
+          />
+        ))}
+      </>
     </Stack>
   );
 };

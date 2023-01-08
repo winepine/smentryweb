72130450fad08fd0bde0b0974b799efb4aadb553
WinePine <basitsaeed1@gmail.com>
2023-01-08 11:08:27 -0800

    searching for activity

--- pages/activity.tsx
+++ pages/activity.tsx
@@ -1,17 +1,33 @@
-import { Box, Container, Heading, Input, Stack } from "@chakra-ui/react";
+import { Box, Container, Heading, HStack, Input, Select, Stack } from "@chakra-ui/react";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { ActivityColumns } from "../components/activity/ActivityColumns";
 import OurTable from "../components/approvals/Table";
 import { StaffColumns } from "../components/staff/StaffColumns";
 import { db } from "../firebase/clientApp";
-
+const constraints = [
+  'name','house','cnic','numberplate'
+]
 const Activity = () => {
   const [activity, setActivity] = useState<any[]>([]);
   const [paginationState, setPaginationState] = useState({
     pageIndex: 0,
     pageSize: 10,
   });
+  const [ogData,setOgData]=useState<any[]>([]);
+  const [constraint,setConstraint]=useState('name');
+  const [searchValue,setsearchValue]=useState('');
+  useEffect(()=>{
+    let newData= ogData;
+    if(searchValue!=''){  
+      if(constraint=='house'){
+        newData = newData.filter((act)=>act.house_no.house.includes(searchValue));
+      }else{
+        newData = newData.filter((act)=>act[constraint].includes(searchValue));
+      }
+    }
+      setActivity(newData)
+  },[searchValue,activity])
   useEffect(() => {
     const colRef = collection(db, "activity");
     const q = query(colRef,orderBy('createdAt','desc'))
@@ -19,15 +35,23 @@ const Activity = () => {
       let activity: any[] = [];
       snapshot.docs.forEach(doc => activity.push({ ...doc.data(), id: doc.id }));
       setActivity(activity);
+      setOgData(activity);
     });
     return unsub;
   }, []);
   return <Container bg="white" minW="100%" minH='98vh' p={0} m={0}>
   <Stack p={8} px={16}>
     <Heading>Activity</Heading>
-    <Box pt={8}>
-      <Input bg="gray.100" placeholder="Search" p={6} />
-    </Box>
+    <HStack pt={8}>
+      <Input minW={'85%'} value={searchValue} onChange={({target})=>setsearchValue(target.value)} bg="gray.100" placeholder="Search" />
+      <Select onChange={({target})=>setConstraint(target.value)}>
+        {
+          constraints.map(con=>
+            <option value={con}>{con[0].toLocaleUpperCase()+con.substring(1,con.length)}</option>
+            )
+        }
+      </Select>
+    </HStack>
     <OurTable
       data={activity}
       columns={ActivityColumns}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import {
 
+  Box,
   Button,
-  Container, Input,
+  Container, Heading, Input, Stack, Toast, useToast,
 
 } from "@chakra-ui/react";
 import Router, { useRouter } from "next/router";
@@ -10,6 +11,7 @@ import { useContext, useEffect, useState } from "react";
 
 export default function Login({setLoggedIn}:any) {
   const router = useRouter()
+  const toast = useToast();
   const [email,setEmail]=useState('')
   const [password,setPassword]=useState('');
   const [showPage,setShowPage]=useState(false);
@@ -25,16 +27,39 @@ export default function Login({setLoggedIn}:any) {
     if(email=='admin' && password=='0316'){
       localStorage.setItem('smentryAdminLoggedIn',"true");
       setLoggedIn(true);
+      toast({
+        title:'Success',
+        status: 'success',
+                isClosable: true,
+                position: "top-right",
+      })
       router.replace('/home');
+    }else{
+      toast({
+        title:'Invalid ID/Pass',
+        status: 'error',
+                isClosable: true,
+                position: "top-right",
+      })
     }
   }
   if(showPage){
 
     return (
-      <Container p={0} m={0}>
-      <Input value={email} onChange={({target})=>setEmail(target.value)} placeholder="Email" />
-      <Input value={password} onChange={({target})=>setPassword(target.value)} placeholder="Password" type='password' />
-      <Button onClick={()=>setAdminLoggedIn()}>Login</Button>
+      <Container minH={'100vh'} bg='gray.100' minW={'100%'} p={0} m={0}>
+        
+<Stack pt={40} align={'center'}>
+  <Box p={24} py={36} bg={'white'} rounded='xl'>
+<Stack>
+
+        <Heading textAlign='center'>Smentry Portal</Heading>
+
+      <Input bg={'gray.100'}  w='xl' value={email} onChange={({target})=>setEmail(target.value)} placeholder="Email" />
+      <Input bg={'gray.100'}  w='xl' value={password} onChange={({target})=>setPassword(target.value)} placeholder="Password" type='password' />
+      <Button colorScheme='green' w='xl'  onClick={()=>setAdminLoggedIn()}>Login</Button>
+</Stack>
+  </Box>
+</Stack>
     </Container>
   );
 }

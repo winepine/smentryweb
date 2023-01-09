6c7b4e3d10e0877e26566ec1f0584c41e21183e8
Basit Saeed <69200255+winepine@users.noreply.github.com>
2023-01-09 13:53:58 +0500

    dynamnic staff

--- components/staff/StaffTableValues.tsx
+++ components/staff/StaffTableValues.tsx
@@ -1,20 +1,33 @@
 import { Tbody, Td, Tr, useDisclosure } from "@chakra-ui/react";
 import { flexRender } from "@tanstack/react-table";
+import { useState } from "react";
 import StaffModal from "./StaffModal";
 
 export function StaffValues({ table }: any) {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedStaff, setSelectedStaff] = useState(null);
   return (
     <Tbody>
+      {console.log(table.getRowModel().rows)}
       {table.getRowModel().rows.map((row: any, index: any) => {
         return (
-          <Tr  key={index} _hover={{ bg: "gray.100" }} onClick={onOpen}>
-            <StaffModal
-              isOpen={isOpen}
-              onOpen={onOpen}
-              onClose={onClose}
-              data={row.getVisibleCells()}
-            />
+          <Tr
+            key={index}
+            _hover={{ bg: "gray.100" }}
+            onClick={() => {
+              setSelectedStaff(row.getVisibleCells());
+              onOpen();
+            }}
+          >
+            {console.log(row.getVisibleCells()[0].getValue())}
+            {selectedStaff && (
+              <StaffModal
+                isOpen={isOpen}
+                onOpen={onOpen}
+                onClose={onClose}
+                data={selectedStaff}
+              />
+            )}
             {row.getVisibleCells().map((cell: any, index: any) => (
               <Td key={index}>
                 {flexRender(cell.column.columnDef.cell, cell.getContext())}

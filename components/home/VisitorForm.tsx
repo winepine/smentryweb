a43519cfd10cefbf0dd93d7b1fdcb5085a6eac07
Basit Saeed <69200255+winepine@users.noreply.github.com>
2023-01-09 13:04:23 +0500

    new block

--- components/home/VisitorForm.tsx
+++ components/home/VisitorForm.tsx
@@ -20,53 +20,55 @@ import { VisitorDataType } from "./types/VisitorDataType";
 const VisitorForm = () => {
   const toast = useToast();
   const [isHouseVisitor, setIsHouseVisitor] = useState<boolean>(false);
-  const [isInvalid,setIsInvalid]=useState({
+  const [isInvalid, setIsInvalid] = useState({
     name: false,
     cnic: false,
     house_no: false,
-    additional: false
-  })
-  const [isLoading,setIsLoading]=useState(false);
+    additional: false,
+  });
+  const [isLoading, setIsLoading] = useState(false);
   const [visitorData, setVisitorData] = useState<VisitorDataType>({
     name: "",
-    cnic: '',
+    cnic: "",
     numberplate: "",
     house_no: "",
     block: "A",
     additional: "",
   });
-  const validateInput = ()=>{
-    
-    let error=false;
-    let errors= {
+  const validateInput = () => {
+    let error = false;
+    let errors = {
       name: false,
       cnic: false,
       house_no: false,
-      additional: false
+      additional: false,
     };
-    if(visitorData.name.length<3){
-      errors={
-        ...errors,name:true
-      }
-      error=true;
+    if (visitorData.name.length < 3) {
+      errors = {
+        ...errors,
+        name: true,
+      };
+      error = true;
     }
-    if(visitorData.cnic.length<13){
-      errors={
-        ...errors,cnic:true
-      }
-      error=true;
+    if (visitorData.cnic.length < 13) {
+      errors = {
+        ...errors,
+        cnic: true,
+      };
+      error = true;
     }
-    if(visitorData.house_no==""){
-      if(visitorData.house_no.length>4){
-        errors={
-          ...errors,house_no:true
-        }
-        error=true;
+    if (visitorData.house_no == "") {
+      if (visitorData.house_no.length > 4) {
+        errors = {
+          ...errors,
+          house_no: true,
+        };
+        error = true;
       }
     }
-    console.log({errors})
-    setIsInvalid(errors)
-    if(error){
+    console.log({ errors });
+    setIsInvalid(errors);
+    if (error) {
       toast({
         title: "Invalid Values.",
         status: "error",
@@ -75,7 +77,7 @@ const VisitorForm = () => {
       });
     }
     return error;
-  }
+  };
   const onChangeFormValueHandler = ({
     target,
   }: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
@@ -85,25 +87,24 @@ const VisitorForm = () => {
     });
   };
   const onFormSubmitHandler = async () => {
-    if(validateInput()){
+    if (validateInput()) {
       return;
     }
     setIsLoading(true);
     await DB_AddVisitorOrRequest(visitorData);
-    if(visitorData.house_no === "") {
+    if (visitorData.house_no === "") {
       await addActivity({
-        name:visitorData.name,
-        cnic:visitorData.cnic,
-        house_no:{
-          house:'',
-          block:''
+        name: visitorData.name,
+        cnic: visitorData.cnic,
+        house_no: {
+          house: "",
+          block: "",
         },
-        numberplate:visitorData.numberplate,
-        type:'visitor',
-        additional:visitorData.additional,
-        createdAt:serverTimestamp()
+        numberplate: visitorData.numberplate,
+        type: "visitor",
+        additional: visitorData.additional,
+        createdAt: serverTimestamp(),
       });
-
     }
     const text =
       visitorData.house_no === ""
@@ -129,16 +130,16 @@ const VisitorForm = () => {
       <HStack py={6} minW="100%" align="start">
         <Stack minW="50%">
           <Input
-          isInvalid={isInvalid.name}
+            isInvalid={isInvalid.name}
             onChange={onChangeFormValueHandler}
             value={visitorData.name}
             name="name"
             placeholder="Name"
           />
           <Input
-          isInvalid={isInvalid.cnic}
-            onChange={({target})=>{
-              if(target.value.length<14){
+            isInvalid={isInvalid.cnic}
+            onChange={({ target }) => {
+              if (target.value.length < 14) {
                 setVisitorData({
                   ...visitorData,
                   [target.name]: target.value,
@@ -146,8 +147,7 @@ const VisitorForm = () => {
               }
             }}
             //as={InputMask}
-            type='number'
-
+            type="number"
             //mask="*************"
             //type='number'
             value={visitorData.cnic}
@@ -166,11 +166,9 @@ const VisitorForm = () => {
           <HStack>
             <Input
               name="house_no"
-              
               isInvalid={isInvalid.house_no}
-
-              onChange={({target})=>{
-                if(target.value.length<5){
+              onChange={({ target }) => {
+                if (target.value.length < 5) {
                   setVisitorData({
                     ...visitorData,
                     [target.name]: target.value,
@@ -179,7 +177,7 @@ const VisitorForm = () => {
               }}
               value={visitorData.house_no}
               placeholder="Where"
-              type='number'
+              type="number"
             />
             <Select
               onChange={onChangeFormValueHandler}
@@ -189,6 +187,7 @@ const VisitorForm = () => {
               <option value="A">A</option>
               <option value="B">B</option>
               <option value="C">C</option>
+              <option value="D">D</option>
             </Select>
           </HStack>
           <Input

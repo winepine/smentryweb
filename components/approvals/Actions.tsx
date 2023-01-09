08b9f92bdf8716293f53556e0f24c6bb01f5c1a1
Basit Saeed <69200255+winepine@users.noreply.github.com>
2023-01-09 12:07:24 +0500

    register house input validation

--- components/approvals/Actions.tsx
+++ components/approvals/Actions.tsx
@@ -5,19 +5,20 @@ import { addActivity } from "../../services/addActivity";
 import { dismissApproval } from "../../services/dismissApproval";
 
 const ActionsForApprovals = ({ approval }: any) => {
+  console.log({ approval });
   const toast = useToast();
-  const [isLoading,setIsLoading] =useState(false);
-  const Submitdismiss = async()=>{
+  const [isLoading, setIsLoading] = useState(false);
+  const Submitdismiss = async () => {
     setIsLoading(true);
     await addActivity({
-      name:approval.name,
-      cnic:approval.cnic,
-      house_no:approval.house_no,
-      numberplate:approval.numberplate,
-      type:'Resident Approved',
-      additional:approval.additional,
-      createdAt:serverTimestamp()
-    })
+      name: approval.name,
+      cnic: approval.cnic,
+      house_no: approval.house_no,
+      numberplate: approval.numberplate,
+      type: "Resident Approved",
+      additional: approval.additional,
+      createdAt: serverTimestamp(),
+    });
     await dismissApproval(approval.id);
     setIsLoading(false);
     toast({
@@ -26,15 +27,20 @@ const ActionsForApprovals = ({ approval }: any) => {
       isClosable: true,
       position: "top-right",
     });
-  }
+  };
   return (
     <>
-    {
-      approval.status==='Approved'&&
-      <Button fontSize='xs' size='xs' isLoading={isLoading} onClick={() => Submitdismiss()} colorScheme="red">
-        Dismiss
-      </Button>
-      }
+      {approval.status === "Approved" && (
+        <Button
+          fontSize="xs"
+          size="xs"
+          isLoading={isLoading}
+          onClick={() => Submitdismiss()}
+          colorScheme="green"
+        >
+          Grant Access
+        </Button>
+      )}
     </>
   );
 };
--- components/houses/RegisterHouseModal.tsx
+++ components/houses/RegisterHouseModal.tsx
@@ -1,112 +1,194 @@
 import {
-    Box,
-    Button,
-    Divider,
-    Heading,
-    HStack,
-    Input,
-    Modal,
-    ModalBody,
-    ModalCloseButton,
-    ModalContent,
-    ModalHeader,
-    ModalOverlay,
-    Stack,
-    Text,
-    useToast,
-  } from "@chakra-ui/react";
+  Box,
+  Button,
+  Divider,
+  Heading,
+  HStack,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
 import { initScriptLoader } from "next/script";
 import { useState } from "react";
 import { addHouse } from "../../services/addHouse";
-  
-  function RegisterHousesModal({ isOpen, onOpen, onClose }: any) {
-    const toast = useToast();
-    const [isLoading,setIsLoading]=useState(false);
-    const [passShow,setPassShow]=useState(false);
-    const [password,setPassword]=useState('')
-    const [houseData, setHouseData] = useState({
-        house_no: "",
-        block: "",
-        owner_name: "",
-        owner_contact: ""
+
+function RegisterHousesModal({ isOpen, onOpen, onClose }: any) {
+  const toast = useToast();
+  const [isLoading, setIsLoading] = useState(false);
+  const [passShow, setPassShow] = useState(false);
+  const [password, setPassword] = useState("");
+  const [houseData, setHouseData] = useState({
+    house_no: "",
+    block: "",
+    owner_name: "",
+    owner_contact: "",
+  });
+  const onChangeFormValueHandler = ({
+    target,
+  }: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    setHouseData({
+      ...houseData,
+      [target.name]: target.value,
+    });
+  };
+  const onFormSubmitHandler = async () => {
+    if (houseData.house_no.length == 0) {
+      toast({
+        title: "Invalid House No.",
+        status: "error",
+        isClosable: true,
+        position: "top-right",
       });
-      const onChangeFormValueHandler = ({
-        target,
-      }: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        setHouseData({
-          ...houseData,
-          [target.name]: target.value,
-        });
-      };
-      const onFormSubmitHandler = async () => {
-        let intialPassword = (Math.random() + 1).toString(36).substring(2);
-        setPassword(intialPassword)
-        setIsLoading(true);
-        if(houseData.block===''){setIsLoading(false);toast({
-            title: "Enter Req Values",
-            status: 'error',
-            isClosable: true,
-            position: "top-right",
-          });;return;}
-        try{
-            const response = await addHouse({...houseData,password:intialPassword});
-            console.log({response})
-            toast({
-                title: "Operation Successful",
-                status: 'success',
-                isClosable: true,
-                position: "top-right",
-              });
-        }
-        catch(x){
-            toast({
-                title: "An Error Has Occured",
-                status: 'error',
-                isClosable: true,
-                position: "top-right",
-              });
-        }
-        finally{
-            setIsLoading(false);
-            setHouseData({
-                ...houseData,
-                owner_name: "",
-        owner_contact: ""
-            })
-            setPassShow(true);
-        }
-      }
-    return (
-      <>
-        <Modal size='3xl' isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader fontSize='2xl' fontWeight='bold' >Add New House</ModalHeader>
-            <Divider />
-            <ModalCloseButton />
-            <ModalBody p={8}>
-            <Stack>
-                <Text textAlign='center' fontSize='lg'>Fill In The Follwing Fields</Text>
-                <Input onChange={onChangeFormValueHandler} name='house_no' value={houseData.house_no} placeholder="House No." />
-                <Input onChange={onChangeFormValueHandler} name='block' value={houseData.block} placeholder="Block" />
-                <Input onChange={onChangeFormValueHandler} name='owner_name' value={houseData.owner_name} placeholder="Owner Name" />
-                <Input onChange={onChangeFormValueHandler} name='owner_contact' value={houseData.owner_contact} placeholder="Owner Contact" />
-                <Box>
-<Stack>
-    {
-passShow&&
-        <Text mt={6} textAlign='center' color='green'>The Initially Created Password For {houseData.house_no}-{houseData.block} is {password}</Text>
+      return;
     }
-                <Button isLoading={isLoading}  colorScheme='green' onClick={()=>onFormSubmitHandler()}>Add House</Button>
-                <Button colorScheme='red' onClick={()=>{setPassShow(false);onClose();}}>Close</Button>
-</Stack>
-                </Box>
+    if (houseData.block.length != 1) {
+      toast({
+        title: "Invalid Block Value",
+        status: "error",
+        isClosable: true,
+        position: "top-right",
+      });
+      return;
+    }
+    if (houseData.owner_name.length == 0) {
+      toast({
+        title: "Invalid Owner Name",
+        status: "error",
+        isClosable: true,
+        position: "top-right",
+      });
+      return;
+    }
+    if (houseData.owner_contact.length < 9) {
+      toast({
+        title: "Invalid Phone No.",
+        status: "error",
+        isClosable: true,
+        position: "top-right",
+      });
+      return;
+    }
+    let intialPassword = (Math.random() + 1).toString(36).substring(2);
+    setPassword(intialPassword);
+    setIsLoading(true);
+    if (houseData.block === "") {
+      setIsLoading(false);
+      toast({
+        title: "Enter Req Values",
+        status: "error",
+        isClosable: true,
+        position: "top-right",
+      });
+      return;
+    }
+    try {
+      const response = await addHouse({
+        ...houseData,
+        password: intialPassword,
+      });
+      console.log({ response });
+      toast({
+        title: "Operation Successful",
+        status: "success",
+        isClosable: true,
+        position: "top-right",
+      });
+    } catch (x) {
+      toast({
+        title: "An Error Has Occured",
+        status: "error",
+        isClosable: true,
+        position: "top-right",
+      });
+    } finally {
+      setIsLoading(false);
+      setHouseData({
+        ...houseData,
+        owner_name: "",
+        owner_contact: "",
+      });
+      setPassShow(true);
+    }
+  };
+  return (
+    <>
+      <Modal size="3xl" isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader fontSize="2xl" fontWeight="bold">
+            Add New House
+          </ModalHeader>
+          <Divider />
+          <ModalCloseButton />
+          <ModalBody p={8}>
+            <Stack>
+              <Text textAlign="center" fontSize="lg">
+                Fill In The Follwing Fields
+              </Text>
+              <Input
+                onChange={onChangeFormValueHandler}
+                name="house_no"
+                value={houseData.house_no}
+                placeholder="House No."
+                type="number"
+              />
+              <Input
+                onChange={onChangeFormValueHandler}
+                name="block"
+                value={houseData.block}
+                placeholder="Block"
+              />
+              <Input
+                onChange={onChangeFormValueHandler}
+                name="owner_name"
+                value={houseData.owner_name}
+                placeholder="Owner Name"
+              />
+              <Input
+                onChange={onChangeFormValueHandler}
+                name="owner_contact"
+                value={houseData.owner_contact}
+                placeholder="Owner Contact"
+                type={"tel"}
+              />
+              <Box pt={6}>
+                <Stack>
+                  {passShow && (
+                    <Text mt={6} textAlign="center" color="green">
+                      The Initially Created Password For {houseData.house_no}-
+                      {houseData.block} is {password}
+                    </Text>
+                  )}
+                  <Button
+                    isLoading={isLoading}
+                    colorScheme="green"
+                    onClick={() => onFormSubmitHandler()}
+                  >
+                    Add House
+                  </Button>
+                  <Button
+                    colorScheme="red"
+                    onClick={() => {
+                      setPassShow(false);
+                      onClose();
+                    }}
+                  >
+                    Close
+                  </Button>
+                </Stack>
+              </Box>
             </Stack>
-            </ModalBody>
-          </ModalContent>
-        </Modal>
-      </>
-    );
-  }
-  export default RegisterHousesModal;
-  
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+export default RegisterHousesModal;
